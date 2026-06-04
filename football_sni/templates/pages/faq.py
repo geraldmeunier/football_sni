cb19d38be08db7d66812c1597ef1e9420ea758b8
gerald <gerald.meunier@daliak.net>
2026-06-04 13:11:46 +0200

    feat: Refactor FAQ category and article sorting for improved clarity

--- football_sni/templates/pages/faq.py
+++ football_sni/templates/pages/faq.py
@@ -34,7 +34,7 @@ def get_faq_categories(selected_article=""):
 				where article.category = category.name
 					and article.published = 1
 			)
-		order by category.help_articles desc, category.category_name asc, category.name asc
+		order by category.category_name desc, category.name desc
 		""",
 		as_dict=True,
 	)
@@ -60,7 +60,7 @@ def get_faq_articles_by_category(selected_article=""):
 		inner join `tabHelp Category` category on category.name = article.category
 		where article.published = 1
 			and category.published = 1
-		order by category.help_articles desc, category.category_name asc, article.level asc, article.creation asc, article.title asc
+		order by category.category_name desc, category.name desc, article.title asc
 		""",
 		as_dict=True,
 	)
